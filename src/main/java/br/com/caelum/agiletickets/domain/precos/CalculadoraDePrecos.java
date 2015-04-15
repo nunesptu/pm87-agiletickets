14bfe464704c9fafa4c7775116c04d1383dae856
altair.cunha <altair.cunha.jr@gmail.com>
2015-04-15 10:59:44 -0300

    quinta refatoracao: parametrizacao da enum tipo de espetaculo para calculo do preco dos ingressos

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -12,26 +12,19 @@ public class CalculadoraDePrecos {
 		Integer totalIngressos = sessao.getTotalIngressos();
 		TipoDeEspetaculo tipo = sessao.getEspetaculo().getTipo();
 		BigDecimal preco = sessao.getPreco();
-		double taxaOcupacao = (totalIngressos - ingressosReservados) / totalIngressos.doubleValue();
+		double taxaOcupacao = (totalIngressos.doubleValue() - ingressosReservados.doubleValue()) / totalIngressos.doubleValue();
 		
-		if(tipo.equals(TipoDeEspetaculo.CINEMA) || tipo.equals(TipoDeEspetaculo.SHOW)) {
-			//quando estiver acabando os ingressos... 
-			if(taxaOcupacao <= 0.05) { 
+		if (taxaOcupacao <= tipo.getLimitePreco())
+			preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(tipo.getAcrescimo())));
+		
+		if (tipo.equals(TipoDeEspetaculo.BALLET) || tipo.equals(TipoDeEspetaculo.ORQUESTRA)){
+			Integer duracaoEmMinutos = sessao.getDuracaoEmMinutos();
+			
+			if(duracaoEmMinutos > 60){
 				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 			}
-		} else {
-			Integer duracaoEmMinutos = sessao.getDuracaoEmMinutos();
-			if(tipo.equals(TipoDeEspetaculo.BALLET) || tipo.equals(TipoDeEspetaculo.ORQUESTRA)) {
-				if(taxaOcupacao <= 0.50) { 
-					preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
-				}
-				
-				if(duracaoEmMinutos > 60){
-					preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
-				}
-			} 
-		} 
-
+		}
+		
 		return preco.multiply(BigDecimal.valueOf(quantidade));
 	}
 
